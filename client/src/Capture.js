f71a690279d1e8168a3ba00fa408b6fc29ca4c01
Dhiraj Basukala <dhiraj.basukala@gmail.com>
2025-02-04 12:24:21 +1100

    show response payload after reqeust completion

--- client/src/Capture.js
+++ client/src/Capture.js
@@ -15,21 +15,21 @@ const Capture = ({
         {captureResponse && !captureResponse.error && (
           <div className="payment-result sb-result-box">
             <p>Capture Result: Added payment method successfully </p>
-            {/* <pre>{JSON.stringify(captureResponse, undefined, 2)}</pre> */}
+            <pre>{JSON.stringify(captureResponse, undefined, 2)}</pre>
           </div>
         )}
 
         {captureResponse && captureResponse.error && (
           <div className="payment-result sb-error-box">
             <p>Capture Result: Add payment method failed </p>
-            {/* <pre>{JSON.stringify(captureResponse, undefined, 2)}</pre> */}
+            <pre>{JSON.stringify(captureResponse, undefined, 2)}</pre>
           </div>
         )}
 
         {chargeResponse && (
           <div className="payment-result sb-result-box">
             <p>Charge Result: successfully charged!</p>
-            {/* <pre>{JSON.stringify(chargeResponse, undefined, 2)}</pre> */}
+            <pre>{JSON.stringify(chargeResponse, undefined, 2)}</pre>
           </div>
         )}
         {!((captureResponse && !captureResponse.error) || chargeResponse || review !== undefined) || (
